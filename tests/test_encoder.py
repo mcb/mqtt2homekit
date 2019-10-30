2143519e87800396517fee481e896143bbd0f638
Matthew Schinckel <matt@schinckel.net>
2019-10-30 14:47:27 +1030

    Fix failing test.

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -8,7 +8,6 @@ from mqtt2homekit.encoder import BridgeEncoder
 
 def test_load_optional_characteristic():
     bridge = MQTTBridge(display_name='Bridge', persist_file='tests/bridge_1.state', mqtt_server=None)
-    assert len(bridge.accessories) == 1
     lightbulb = bridge.accessories[2].get_service('Lightbulb')
     assert lightbulb.characteristics[1].display_name == 'Brightness'
 
